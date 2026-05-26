5ac8c8575928a24da67e1641981e4ed8e2d013b1
bonniesdad <bonniesnerdydad@gmail.com>
2026-05-26 08:05:40 +0100

    Logging

--- Functions/Verification/AmIVerified.lua
+++ Functions/Verification/AmIVerified.lua
@@ -1,4 +1,5 @@
 function FreshSoD_AmIVerified()
+    print('AmIVerified: ' .. tostring(not FreshSoD_GetDBValue('playerMoneyValidationFailed')) .. ' ' .. tostring(not FreshSoD_GetDBValue('buffValidationFailed')) .. ' ' .. tostring(FreshSoD_GetDBValue('buffVerifiedDisabled')))
     return not FreshSoD_GetDBValue('playerMoneyValidationFailed')
         and not FreshSoD_GetDBValue('buffValidationFailed')
         and FreshSoD_GetDBValue('buffVerifiedDisabled')
--- Functions/Verification/CanPerformTradeWithPlayer.lua
+++ Functions/Verification/CanPerformTradeWithPlayer.lua
@@ -4,9 +4,11 @@ function FreshSoD_CanPerformTradeWithPlayer(playerName, onComplete)
   end
 
   if not FreshSoD_IsTradePartnerInMyGuild() then
-    onComplete(false, 'Trade with ' .. playerName .. ' blocked - not on my Guild.')
+    onComplete(false, 'Trade with ' .. playerName .. ' blocked - not in my Guild.')
     return
   end
+
+  FreshSoD_PrintRestrictionMessage(playerName .. ' is in my Guild.' .. ' Starting verification...')
 
   FreshSoD_BeginTradeVerification(playerName, onComplete)
 end
